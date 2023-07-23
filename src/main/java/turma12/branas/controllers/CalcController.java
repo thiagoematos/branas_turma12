e7edcae4151e943788e3968b0ca95f0972f04e0f
Thiago Matos <thiagoematos@gmail.com>
2023-07-23 20:21:50 -0300

    código correto

--- src/main/java/turma12/branas/controllers/CalcController.java
+++ src/main/java/turma12/branas/controllers/CalcController.java
@@ -5,70 +5,26 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import turma12.branas.exceptions.InvalidDateException;
+import turma12.branas.exceptions.InvalidDistanceException;
+import turma12.branas.models.Input;
+import turma12.branas.models.Movement;
+import turma12.branas.models.Ride;
 
-import java.util.Date;
-
-class Mov {
-    public String ds;
-    public Integer dist;
-}
-
-class Req {
-    public Mov[] body;
-}
+import static org.springframework.http.ResponseEntity.badRequest;
+import static org.springframework.http.ResponseEntity.ok;
 
 @Slf4j
 @RestController
 public class CalcController {
-    @PostMapping(value = "/calc")
-    public ResponseEntity<?> calc(@RequestBody Req req) {
-        var result = 0.0d;
-        for (var mov : req.body) {
-
-            var ds = new Date(mov.ds);
-
-            if (mov.dist != null && mov.dist > 0) {
-
-                if (mov.ds != null) {
-
-                    // overnight
-                    if (ds.getHours() >= 22 || ds.getHours() <= 6) {
-
-                        // not sunday
-                        if (ds.getDay() != 0) {
-
-                            result += mov.dist * 3.90;
-                            // sunday
-                        } else {
-                            result += mov.dist * 5;
-
-                        }
-
-                    } else {
-                        // sunday
-                        if (ds.getDay() == 0) {
-
-                            result += mov.dist * 2.9;
-                        } else {
-                            result += mov.dist * 2.10;
-                        }
-
-                    }
-
-                } else {
-                    // System.out.println(d);
-                    return ResponseEntity.ok(-2);
-                }
-
-            } else {
-
-                // System.out.println(req.body.dist);
-                return ResponseEntity.ok(-1);
-            }
+    @PostMapping(value = "/calculate-price")
+    public ResponseEntity<?> calculatePrice(@RequestBody Input request) {
+        try {
+            var movements = request.movements();
+            var segments = movements.stream().map(Movement::toSegment).toList();
+            return ok(Ride.getPrice(segments));
+        } catch (InvalidDistanceException | InvalidDateException e) {
+            return badRequest().body(e.getMessage());
         }
-        if (result < 10) {
-            result = 10;
-        }
-        return ResponseEntity.ok(result);
     }
 }
